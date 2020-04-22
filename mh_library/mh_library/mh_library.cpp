69c1a4dd4f12a077985f5903f1b961fdc497a811
gdgdkuro <gdgdkuro@gmail.com>
2020-04-22 21:22:01 +0900

    OutputConsoleを追加

--- mh_library/mh_library/mh_library.cpp
+++ mh_library/mh_library/mh_library.cpp
@@ -1,3 +1,12 @@
-﻿int main() {
+﻿#include "mh_library/util/output/IOutputConsole.hpp"
+#include "mh_library/util/output/OutputConsole.hpp"
+
+int main() {
+    mh_library::IOutputConsole* console = new mh_library::OutputConsole();
+    console->Print("HelloWorld");
+    if (console) {
+        delete console;
+        console = NULL;
+    }
     return 0;
 }
---
+++ mh_library/mh_library/mh_library/util/output/OutputConsole.cpp
@@ -0,0 +1,29 @@
+﻿#include "OutputConsole.hpp"
+
+#include <iostream>
+
+/**
+ * コンストラクタ
+ */
+mh_library::OutputConsole::OutputConsole() {
+}
+
+/**
+ * デストラクタ
+ */
+mh_library::OutputConsole::~OutputConsole() {
+}
+
+/**
+ * 文字列を出力する
+ */
+void mh_library::OutputConsole::Print(std::string string) {
+	std::cout << string;
+}
+
+/**
+ * 改行付き文字列を出力する
+ */
+void mh_library::OutputConsole::PrintLine(std::string string) {
+	std::cout << string << std::endl;
+}
