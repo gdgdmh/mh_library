70157279ccff23b8010c7d63847aa9826f2b884a
gdgdkuro <gdgdkuro@gmail.com>
2020-04-23 14:10:55 +0900

    UnitTestExceptionの使い方コメントを追加

--- mh_library/mh_library/mh_library.cpp
+++ mh_library/mh_library/mh_library.cpp
@@ -1,12 +1,20 @@
-﻿#include "mh_library/util/output/IOutputConsole.hpp"
+﻿#include <memory>
+#include "mh_library/util/output/IOutputConsole.hpp"
 #include "mh_library/util/output/OutputConsole.hpp"
+#include "mh_library/exception/UnitTestException.hpp"
 
 int main() {
-  mh_library::IOutputConsole* console = new mh_library::OutputConsole();
-  console->Print("HelloWorld");
-  if (console) {
-    delete console;
-    console = NULL;
+  std::shared_ptr<mh_library::IOutputConsole> console(new mh_library::OutputConsole());
+  console->PrintLine("HelloWorld");
+  // UnitTestExceptionTest
+  try {
+      int value = 1000;
+      if (value > 10) {
+          throw mh_library::UnitTestException("value error", 1);
+      }
+  } catch (const mh_library::UnitTestException& e) {
+    console->PrintLine(e.what());
   }
+
   return 0;
 }
--- mh_library/mh_library/mh_library/exception/UnitTestException.hpp
+++ mh_library/mh_library/mh_library/exception/UnitTestException.hpp
@@ -5,6 +5,21 @@
 
 namespace mh_library {
 
+// 使い方
+// std::shared_ptr<mh_library::IOutputConsole> console(new mh_library::OutputConsole());
+// // UnitTestExceptionTest
+// try {
+//   int value = 1000;
+//   if (value > 10) {
+//     // パラメータエラーが起こったと仮定して例外をスロー
+//     throw mh_library::UnitTestException("value error", 1);
+//   }
+// }
+// catch (const mh_library::UnitTestException& e) {
+//   // e.what()でメッセージを出力
+//   console->PrintLine(e.what());
+// }
+
 // 単体テスト用の例外クラス
 class UnitTestException : public std::runtime_error {
 public:
