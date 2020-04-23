75e63b4bce89474dae1a4b7f85709139bf018dc4
gdgdkuro <gdgdkuro@gmail.com>
2020-04-23 13:43:50 +0900

    ユニットテスト例外クラスを追加

---
+++ mh_library/mh_library/mh_library/exception/UnitTestException.hpp
@@ -0,0 +1,24 @@
+﻿#ifndef MH_LIBRABY_EXCEPTION_UNITTESTEXCEPTION_H_
+#define MH_LIBRABY_EXCEPTION_UNITTESTEXCEPTION_H_
+
+#include <stdexcept>
+
+namespace mh_library {
+
+// 単体テスト用の例外クラス
+class UnitTestException : public std::runtime_error {
+public:
+  UnitTestException(const std::string& message, int error_code);
+
+  virtual ~UnitTestException();
+
+  int GetResult() {
+    return error_code_;
+  }
+private:
+  int error_code_; // エラーコード情報
+};
+
+}
+
+#endif  // MH_LIBRABY_EXCEPTION_UNITTESTEXCEPTION_H_
