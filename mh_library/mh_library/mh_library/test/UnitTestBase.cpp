001618004f7241256922d30d558e10f4d1cb35d0
gdgdkuro <gdgdkuro@gmail.com>
2020-04-23 11:58:10 +0900

    ユニットテストベースクラスを追加

---
+++ mh_library/mh_library/mh_library/test/UnitTestBase.cpp
@@ -0,0 +1,42 @@
+﻿#include "UnitTestBase.hpp"
+
+/**
+ * コンストラクタ
+ * @param outputConsole コンソール出力クラス
+ */
+mh_library::UnitTestBase::UnitTestBase(std::shared_ptr<mh_library::IOutputConsole> output_console) :
+	output_console_(output_console) {
+}
+
+/**
+ * デストラクタ
+ */
+mh_library::UnitTestBase::~UnitTestBase() {
+}
+
+/**
+ * 実行
+ * @return trueなら成功 falseなら失敗
+ */
+bool mh_library::UnitTestBase::Execute() {
+	return true;
+}
+
+/**
+ * 条件のチェックの成否をチェックして失敗したときはメッセージを表示する
+ * @param expected 条件チェックした値(0なら失敗とする)
+ * @param message  失敗した際のメッセージ
+ * @return          falseなら失敗
+ */
+bool mh_library::UnitTestBase::AssertEquals(int expected, std::string message) {
+	if (expected != 0) {
+		// 正常
+		return true;
+	}
+	if (output_console_) {
+		// 失敗しているのでメッセージを出力する
+		// 後に__LINE__や__FILE__を追加することも考慮する
+		output_console_->PrintLine(message);
+	}
+	return false;
+}
---
+++ mh_library/mh_library/mh_library/test/UnitTestBase.hpp
@@ -0,0 +1,47 @@
+﻿#ifndef MH_LIBRABY_TEST_UNITTESTBASE_H_
+#define MH_LIBRABY_TEST_UNITTESTBASE_H_
+
+#include <string>
+#include <memory>
+#include "../util/output/IOutputConsole.hpp"
+
+namespace mh_library {
+
+// 単体テスト用のベースクラス
+class UnitTestBase {
+public:
+  /**
+   * コンストラクタ
+   * @param outputConsole コンソール出力クラス
+   */
+  UnitTestBase(std::shared_ptr<mh_library::IOutputConsole> output_console);
+
+  /**
+   * デストラクタ
+   */
+  virtual ~UnitTestBase();
+
+  /**
+   * 実行
+   * @return trueなら成功 falseなら失敗
+   */
+  virtual bool Execute();
+
+protected:
+
+  /**
+   * 条件のチェックの成否をチェックして失敗したときはメッセージを表示する
+   * メソッド内ではプログラム停止をしないので注意
+   * @param expected 条件チェックした値(0なら失敗とする)
+   * @param message  失敗した際のメッセージ
+   * @return          falseなら失敗
+   */
+  bool AssertEquals(int expected, std::string message);
+
+protected:
+  std::shared_ptr<mh_library::IOutputConsole> output_console_; // コンソール出力
+};
+
+}
+
+#endif  // MH_LIBRABY_TEST_UNITTESTBASE_H_
