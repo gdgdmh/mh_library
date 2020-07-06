8401a82d0cd879c10d91fe43bc6f03a62e5d8508
gdgdkuro <gdgdkuro@gmail.com>
2020-07-06 11:20:37 +0900

    コードフォーマットをGoogleStyleに変更 一部にUTF-8 withBOMでないファイルがあったのを修正

--- mh_library/mh_library/mh_library.cpp
+++ mh_library/mh_library/mh_library.cpp
@@ -1,22 +1,26 @@
 ﻿#include <memory>
-#include "mhl/util/output/IOutputConsole.hpp"
-#include "mhl/util/output/OutputConsole.hpp"
+
 #include "mhl/exception/UnitTestException.hpp"
 #include "mhl/test/UnitTestExecuteList.hpp"
+#include "mhl/util/output/IOutputConsole.hpp"
+#include "mhl/util/output/OutputConsole.hpp"
 #include "test/TestCppNewFunctionTest.hpp"
 #include "test/TestExecuteMethod.hpp"
-#include "test/TestSubject.hpp"
 #include "test/TestInstanceOfTest.hpp"
+#include "test/TestSubject.hpp"
 
 int main() {
   mhl::UnitTestExecuteList list;
   std::shared_ptr<mhl::IOutputConsole> console(new mhl::OutputConsole());
-  list.Add(std::shared_ptr<test::TestCppNewFunctionTest>(new test::TestCppNewFunctionTest(console)));
-  list.Add(std::shared_ptr<test::TestExecuteMethod>(new test::TestExecuteMethod(console)));
+  list.Add(std::shared_ptr<test::TestCppNewFunctionTest>(
+      new test::TestCppNewFunctionTest(console)));
+  list.Add(std::shared_ptr<test::TestExecuteMethod>(
+      new test::TestExecuteMethod(console)));
   list.Add(std::shared_ptr<test::TestSubject>(new test::TestSubject(console)));
-  list.Add(std::shared_ptr<test::TestInstanceOfTest>(new test::TestInstanceOfTest(console)));
+  list.Add(std::shared_ptr<test::TestInstanceOfTest>(
+      new test::TestInstanceOfTest(console)));
   if (!list.Execute()) {
-      return 1;
+    return 1;
   }
   return 0;
 }
