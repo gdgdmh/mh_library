a69e9b75601cb0c2d76daae87f961b99e32fa6f3
gdgdkuro <gdgdkuro@gmail.com>
2020-08-13 16:49:07 +0900

    コールバックの検証テストを追加

--- mh_library/mh_library/mh_library.cpp
+++ mh_library/mh_library/mh_library.cpp
@@ -2,9 +2,15 @@
 
 #include "mhl/test_code/test_mhl.hpp"
 #include "mhl/util/output/output_console.hpp"
+// test
+#include "mhl/test_code/test_callback_test.hpp"
 
 int main() {
   std::shared_ptr<mhl::IOutputConsole> output(new mhl::OutputConsole());
   test::TestMhl test(output);
   test.ExecuteUnitTest();
+
+  output->PrintLine("--- main test -------------------------------------------------------");
+  test::TestCallbackTest ct(output);
+  ct.ExecuteUnitTest();
 }
---
+++ mh_library/mh_library/mhl/test_code/test_callback_test.cpp
@@ -0,0 +1,56 @@
+﻿#include "test_callback_test.hpp"
+
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+test::CallbackInner::CallbackInner() {}
+
+test::CallbackInner::~CallbackInner() {}
+
+void test::CallbackInner::Execute(CallbackInnerComplete callback) {
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000 * 5));
+  std::cout << "inner";
+  if (callback != nullptr) {
+    callback(1, 0);
+  }
+}
+
+test::CallbackOuter::CallbackOuter() {}
+test::CallbackOuter::~CallbackOuter() {}
+
+void test::CallbackOuter::Execute(CallbackOuterComplete callback) {
+  test::CallbackInner inner;
+  inner.Execute([callback](uint32_t p1, uint32_t p2) {
+    if ((p1 == 0) && (p2 == 0)) {
+      callback(true);
+    } else {
+      callback(false);
+    }
+  });
+}
+
+/**
+ * コンストラクタ
+ */
+test::TestCallbackTest::TestCallbackTest(
+    std::shared_ptr<mhl::IOutputConsole> output_console)
+    : UnitTestBase(output_console) {}
+
+/**
+ * デストラクタ
+ */
+test::TestCallbackTest::~TestCallbackTest() {}
+
+void test::TestCallbackTest::ExecuteUnitTest() {
+  test::CallbackOuter outer;
+  outer.Execute([](bool result) {
+    if (result) {
+      std::cout << "success";
+    } else {
+      std::cout << "failure";
+    }
+  });
+
+  // TestCpp11Nullptr();
+}
