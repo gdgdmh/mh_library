ed4e97019038df7c722250c509426045628245f6
gdgdkuro <gdgdkuro@gmail.com>
2020-08-23 23:54:30 +0900

    2分木検索用のテストコードを追加

---
+++ mh_library/mh_library/mhl/test_code/test_binarytree_test.cpp
@@ -0,0 +1,15 @@
+﻿#include "test_binarytree_test.hpp"
+
+/**
+ * コンストラクタ
+ */
+test::TestBinaryTest::TestBinaryTest(
+    std::shared_ptr<mhl::IOutputConsole> output_console)
+    : UnitTestBase(output_console) {}
+
+/**
+ * デストラクタ
+ */
+test::TestBinaryTest::~TestBinaryTest() {}
+
+void test::TestBinaryTest::ExecuteUnitTest() {}
---
+++ mh_library/mh_library/mhl/test_code/test_binarytree_test.hpp
@@ -0,0 +1,28 @@
+﻿#ifndef MHL_TEST_CODE_TEST_BINARYTREE_TEST_HPP_
+#define MHL_TEST_CODE_TEST_BINARYTREE_TEST_HPP_
+
+#include <memory>
+
+#include "../test/unittest_base.hpp"
+
+namespace test {
+
+// 2分木テスト
+class TestBinaryTest : public mhl::UnitTestBase {
+ public:
+  /**
+   * コンストラクタ
+   */
+  TestBinaryTest(std::shared_ptr<mhl::IOutputConsole> output_console);
+
+  /**
+   * デストラクタ
+   */
+  virtual ~TestBinaryTest();
+
+  void ExecuteUnitTest();
+};
+
+}  // namespace test
+
+#endif  // MHL_TEST_CODE_TEST_BINARYTREE_TEST_HPP_
