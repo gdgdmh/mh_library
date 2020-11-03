7e87677cffc697126a17588cd24aa46712ec3ef4
gdgdkuro <gdgdkuro@gmail.com>
2020-11-03 16:59:25 +0900

    std::moveのテストを追加

---
+++ mh_library/mh_library/mhl/test_program/test_move_test.cpp
@@ -0,0 +1,64 @@
+﻿#include "test_move_test.hpp"
+
+/**
+ * @brief Construct a new Test Move Dummy object
+ *
+ * @param output_console
+ */
+test_program::TestMoveDummy::TestMoveDummy(
+    std::shared_ptr<mhl::IOutputConsole>& output_console) {
+  output_console_ = output_console;
+  output_console_->PrintLine("TestMoveDummy Constructor");
+}
+
+/**
+ * コンストラクタ
+ */
+test_program::TestMoveTest::TestMoveTest(
+    std::shared_ptr<mhl::IOutputConsole> output_console)
+    : UnitTestBase(output_console) {}
+
+/**
+ * デストラクタ
+ */
+test_program::TestMoveTest::~TestMoveTest() {}
+
+/**
+ * @brief テスト実行
+ *
+ */
+void test_program::TestMoveTest::ExecuteUnitTest() {
+  output_console_->PrintLine("--- TestMoveTest ---");
+  TestClassConstractor();
+  TestVector();
+  output_console_->PrintLine("------------------------");
+}
+
+/**
+ * @brief クラスコンストラクタが呼ばれないテスト
+ *
+ */
+void test_program::TestMoveTest::TestClassConstractor() {
+  output_console_->Print("dummy1 ");
+  std::shared_ptr<TestMoveDummy> dummy1(new TestMoveDummy(output_console_));
+  // dummy2がmoveされたときTestMoveDummyのコンストラクタは呼ばれない
+  // dummy1とdummy2は同じポインタを指してる
+  std::shared_ptr<TestMoveDummy>&& dummy2 = std::move(dummy1);
+  AssertEquals(dummy1 == dummy2, "dummy1 not equals dummy2");
+}
+
+/**
+ * @brief vectorでのテスト
+ *
+ */
+void test_program::TestMoveTest::TestVector() {
+  std::vector<TestMoveDummy> dummyV;
+  output_console_->Print("dummyA ");
+  TestMoveDummy dummyA(output_console_);
+  dummyV.emplace_back(dummyA);
+  output_console_->Print("dummyB ");
+  TestMoveDummy dummyB(output_console_);
+  dummyV.emplace_back(dummyB);
+  // vectorのコピーが発生しない
+  std::vector<TestMoveDummy>&& tmp = std::move(dummyV);
+}
---
+++ mh_library/mh_library/mhl/test_program/test_move_test.hpp
@@ -0,0 +1,65 @@
+﻿#ifndef MHL_TEST_PROGRAM_TEST_MOVE_TEST_HPP_
+#define MHL_TEST_PROGRAM_TEST_MOVE_TEST_HPP_
+
+#include <memory>
+
+#include "../test/unittest_base.hpp"
+#include "../util/string/std_string_formatter.hpp"
+
+namespace test_program {
+
+class TestMoveDummy {
+ public:
+  /**
+   * @brief Construct a new Test Move Dummy object
+   *
+   * @param output_console
+   */
+  TestMoveDummy(std::shared_ptr<mhl::IOutputConsole>& output_console);
+
+ private:
+  std::shared_ptr<mhl::IOutputConsole> output_console_;
+};
+
+/**
+ * @brief std::moveテスト
+ *
+ */
+class TestMoveTest : public mhl::UnitTestBase {
+ public:
+  /**
+   * @brief Construct a new Test Move Test object
+   *
+   * @param output_console
+   */
+  TestMoveTest(std::shared_ptr<mhl::IOutputConsole> output_console);
+
+  /**
+   * @brief Destroy the Test Move Test object
+   *
+   */
+  virtual ~TestMoveTest();
+
+  /**
+   * @brief テスト実行
+   *
+   */
+  void ExecuteUnitTest();
+
+ private:
+  /**
+   * @brief クラスコンストラクタが呼ばれないテスト
+   *
+   */
+  void TestClassConstractor();
+
+  /**
+   * @brief vectorでのテスト
+   *
+   */
+  void TestVector();
+};
+
+}  // namespace test_program
+
+#endif  // MHL_TEST_PROGRAM_TEST_MOVE_TEST_HPP_
