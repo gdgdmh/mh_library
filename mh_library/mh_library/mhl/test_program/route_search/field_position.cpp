be818cb97c9c8c11c064f5069a055e2e94f336cd
gdgdkuro <gdgdkuro@gmail.com>
2021-02-02 22:12:42 +0900

    フィールド位置情報のコード整理

--- mh_library/mh_library/mhl/test_program/route_search/field_position.cpp
+++ mh_library/mh_library/mhl/test_program/route_search/field_position.cpp
@@ -1,1 +1,22 @@
 ﻿#include "field_position.hpp"
+
+/**
+ * @brief Construct a new Field Position object
+ *
+ */
+test_program::route_search::FieldPosition::FieldPosition() : x_(0), y_(0) {}
+
+/**
+ * @brief Construct a new Field Position object
+ *
+ * @param x 位置X
+ * @param y 位置Y
+ */
+test_program::route_search::FieldPosition::FieldPosition(int32_t x, int32_t y)
+    : x_(x), y_(y) {}
+
+/**
+ * @brief Destroy the Field Position object
+ *
+ */
+test_program::route_search::FieldPosition::~FieldPosition() {}
--- mh_library/mh_library/mhl/test_program/route_search/field_position.hpp
+++ mh_library/mh_library/mhl/test_program/route_search/field_position.hpp
@@ -9,23 +9,60 @@ namespace test_program {
 
 namespace route_search {
 
-// 位置情報
+/**
+ * @brief フィールド位置情報
+ *
+ */
 class FieldPosition {
  public:
-  FieldPosition() : x_(0), y_(0) {}
-  FieldPosition(int32_t x, int32_t y) : x_(x), y_(y) {}
-  virtual ~FieldPosition() {}
+  /**
+   * @brief Construct a new Field Position object
+   *
+   */
+  FieldPosition();
 
+  /**
+   * @brief Construct a new Field Position object
+   *
+   * @param x 位置X
+   * @param y 位置Y
+   */
+  FieldPosition(int32_t x, int32_t y);
+
+  /**
+   * @brief Destroy the Field Position object
+   *
+   */
+  virtual ~FieldPosition();
+
+  /**
+   * @brief Set the Position object
+   *
+   * @param x 位置X
+   * @param y 位置Y
+   */
   void SetPosition(int32_t x, int32_t y) {
     x_ = x;
     y_ = y;
   }
+
+  /**
+   * @brief 位置Xの取得
+   *
+   * @return int32_t 位置
+   */
   int32_t GetX() const { return x_; }
+
+  /**
+   * @brief 位置Yの取得
+   *
+   * @return int32_t 位置
+   */
   int32_t GetY() const { return y_; }
 
  private:
-  int32_t x_;
-  int32_t y_;
+  int32_t x_;  // 位置X
+  int32_t y_;  // 位置Y
 };
 
 }  // namespace route_search
