0283df390eef97cf0cb87f6e4a5b754d6fdf22b2
gdgdkuro <gdgdkuro@gmail.com>
2021-02-02 22:25:39 +0900

    フィールドのコード整理

--- mh_library/mh_library/mhl/test_program/route_search/field.cpp
+++ mh_library/mh_library/mhl/test_program/route_search/field.cpp
@@ -1,1 +1,71 @@
 ﻿#include "field.hpp"
+
+/**
+ * @brief Construct a new Field object
+ *
+ */
+test_program::route_search::Field::Field() {}
+
+/**
+ * @brief フィールド生成
+ *
+ * @param fieldXSize 横方向サイズ
+ * @param fieldYSize 縦方向サイズ
+ */
+void test_program::route_search::Field::Initialize(uint32_t fieldXSize,
+                                                   uint32_t fieldYSize) {
+  field_.resize(fieldYSize);
+  for (std::vector<FieldInfo>& info : field_) {
+    info.resize(fieldXSize);
+  }
+  // 位置初期値入れる
+  SetPosition();
+  // タイプをすべてkNoneに設定
+  SetAllTypeNone();
+}
+
+/**
+ * @brief フィールドの状態を出力
+ *
+ * @param output_console
+ */
+void test_program::route_search::Field::Print(
+    const std::shared_ptr<mhl::IOutputConsole>& output_console) {
+  for (const auto& y : field_) {
+    std::string line = "";
+    for (const auto& x : y) {
+      line += x.GetType().ToString();
+    }
+    output_console->PrintLine(line);
+  }
+}
+
+/**
+ * @brief 位置を設定
+ *
+ */
+void test_program::route_search::Field::SetPosition() {
+  size_t yLength = field_.size();
+  for (size_t y = 0; y < yLength; ++y) {
+    auto& yInfo = field_[y];
+    for (size_t x = 0; x < yInfo.size(); ++x) {
+      yInfo[x].SetPosition(test_program::route_search::FieldPosition(
+          static_cast<int32_t>(x), static_cast<int32_t>(y)));
+    }
+  }
+}
+
+/**
+ * @brief 全てのフィールドのタイプをNoneに設定
+ *
+ */
+void test_program::route_search::Field::SetAllTypeNone() {
+  size_t yLength = field_.size();
+  for (size_t y = 0; y < yLength; ++y) {
+    auto& yInfo = field_[y];
+    for (size_t x = 0; x < yInfo.size(); ++x) {
+      yInfo[x].SetType(test_program::route_search::FieldType(
+          test_program::route_search::FieldType::Type::kNone));
+    }
+  }
+}
--- mh_library/mh_library/mhl/test_program/route_search/field.hpp
+++ mh_library/mh_library/mhl/test_program/route_search/field.hpp
@@ -16,7 +16,11 @@ namespace route_search {
 // フィールド全体情報
 class Field {
  public:
-  Field() {}
+  /**
+   * @brief Construct a new Field object
+   *
+   */
+  Field();
 
   /**
    * @brief フィールド生成
@@ -24,49 +28,27 @@ class Field {
    * @param fieldXSize 横方向サイズ
    * @param fieldYSize 縦方向サイズ
    */
-  void Initialize(uint32_t fieldXSize, uint32_t fieldYSize) {
-    field_.resize(fieldYSize);
-    for (std::vector<FieldInfo>& info : field_) {
-      info.resize(fieldXSize);
-    }
-    // 位置初期値入れる
-    SetPosition();
-    // タイプをすべてkNoneに設定
-    SetAllTypeNone();
-  }
+  void Initialize(uint32_t fieldXSize, uint32_t fieldYSize);
 
-  void Print(const std::shared_ptr<mhl::IOutputConsole>& output_console) {
-    for (const auto& y : field_) {
-      std::string line = "";
-      for (const auto& x : y) {
-        line += x.GetType().ToString();
-      }
-      output_console->PrintLine(line);
-    }
-  }
+  /**
+   * @brief フィールドの状態を出力
+   *
+   * @param output_console
+   */
+  void Print(const std::shared_ptr<mhl::IOutputConsole>& output_console);
 
  private:
-  void SetPosition() {
-    size_t yLength = field_.size();
-    for (size_t y = 0; y < yLength; ++y) {
-      auto& yInfo = field_[y];
-      for (size_t x = 0; x < yInfo.size(); ++x) {
-        yInfo[x].SetPosition(test_program::route_search::FieldPosition(
-            static_cast<int32_t>(x), static_cast<int32_t>(y)));
-      }
-    }
-  }
+  /**
+   * @brief 位置を設定
+   *
+   */
+  void SetPosition();
 
-  void SetAllTypeNone() {
-    size_t yLength = field_.size();
-    for (size_t y = 0; y < yLength; ++y) {
-      auto& yInfo = field_[y];
-      for (size_t x = 0; x < yInfo.size(); ++x) {
-        yInfo[x].SetType(test_program::route_search::FieldType(
-            test_program::route_search::FieldType::Type::kNone));
-      }
-    }
-  }
+  /**
+   * @brief 全てのフィールドのタイプをNoneに設定
+   *
+   */
+  void SetAllTypeNone();
 
  private:
   std::vector<std::vector<FieldInfo> > field_;  // x,yの二次元配列
