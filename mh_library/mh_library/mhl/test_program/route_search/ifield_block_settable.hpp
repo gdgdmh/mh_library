b59813d8d370c92f1da4fbc11d0ce8106ebb67bd
gdgdkuro <gdgdkuro@gmail.com>
2021-02-02 22:41:06 +0900

    ブロック設定IFの名前を変更、コード整理

---
+++ mh_library/mh_library/mhl/test_program/route_search/ifield_block_settable.hpp
@@ -0,0 +1,38 @@
+﻿#ifndef MHL_TEST_PROGRAM_ROUTE_SEARCH_IFIELD_BLOCK_CHANGEABLE_HPP_
+#define MHL_TEST_PROGRAM_ROUTE_SEARCH_IFIELD_BLOCK_CHANGEABLE_HPP_
+
+#include <stdint.h>
+
+#include <memory>
+
+#include "field.hpp"
+
+namespace test_program {
+
+namespace route_search {
+
+/**
+ * @brief フィールドにブロックを変更(設定)するIFクラス
+ *
+ */
+class IFieldBlockSettable {
+ public:
+  /**
+   * @brief Destroy the IFieldBlockChangeable object
+   *
+   */
+  virtual ~IFieldBlockSettable() {}
+
+  /**
+   * @brief フィールドのブロック
+   *
+   * @param field
+   */
+  virtual void SetBlock(test_program::route_search::FieldType& field) = 0;
+};
+
+}  // namespace route_search
+
+}  // namespace test_program
+
+#endif  // MHL_TEST_PROGRAM_ROUTE_SEARCH_IFIELD_BLOCK_CHANGEABLE_HPP_
