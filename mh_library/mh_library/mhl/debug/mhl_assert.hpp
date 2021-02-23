ef9dd7cc4659456416b4b5289b30bad663309243
gdgdkuro <gdgdkuro@gmail.com>
2021-02-23 22:10:26 +0900

    assertを仮追加

---
+++ mh_library/mh_library/mhl/debug/mhl_assert.hpp
@@ -0,0 +1,19 @@
+﻿#ifndef MHL_DEBUG_MHL_ASSERT_HPP_
+#define MHL_DEBUG_MHL_ASSERT_HPP_
+
+#include <cassert>
+
+namespace mhl {
+
+namespace debug {
+
+#ifndef MHL_ASSERT
+// 一時しのぎのassert
+#define MHL_ASSERT(expr, msg) _ASSERT_EXPR(expr, msg)
+#endif  // MHL_ASSERT
+
+}  // namespace debug
+
+}  // namespace mhl
+
+#endif  // MHL_DEBUG_MHL_ASSERT_HPP_
