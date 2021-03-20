980e20199c4d26f79f3b61f55c47e71a57786c6d
gdgdkuro <gdgdkuro@gmail.com>
2021-03-20 16:46:37 +0900

    テストファイルの名前をリネーム、VSのコンソール出力を追加

---
+++ mh_library/mh_library/mhl/output/console/output_console_vs_sjis.cpp
@@ -0,0 +1,33 @@
+﻿#include "output_console_vs_sjis.hpp"
+
+#ifdef _MSC_VER
+#include <windows.h>
+#endif  // _MSC_VER
+
+/**
+ * コンストラクタ
+ */
+mhl::output::console::OutputConsoleVsSjis::OutputConsoleVsSjis() {}
+
+/**
+ * デストラクタ
+ */
+mhl::output::console::OutputConsoleVsSjis::~OutputConsoleVsSjis() {}
+
+/**
+ * 文字列を出力する
+ */
+void mhl::output::console::OutputConsoleVsSjis::Print(std::string string) {
+#ifdef _MSC_VER
+  OutputDebugStringA(string.c_str());
+#endif  // _MSC_VER
+}
+
+/**
+ * 改行付き文字列を出力する
+ */
+void mhl::output::console::OutputConsoleVsSjis::PrintLine(std::string string) {
+#ifdef _MSC_VER
+  OutputDebugStringA(string.c_str());
+#endif  // _MSC_VER
+}
---
+++ mh_library/mh_library/mhl/output/console/output_console_vs_sjis.hpp
@@ -0,0 +1,47 @@
+﻿#ifndef MHL_OUTPUT_CONSOLE_OUTPUTCONSOLE_VS_SJIS_HPP_
+#define MHL_OUTPUT_CONSOLE_OUTPUTCONSOLE_VS_SJIS_HPP_
+
+#include <string>
+
+#include "ioutput_console.hpp"
+
+namespace mhl {
+
+namespace output {
+
+namespace console {
+
+/**
+ * @brief VisualStudioの出力ウィンドウに出力
+ *
+ */
+class OutputConsoleVsSjis : public mhl::output::console::IOutputConsole {
+ public:
+  /**
+   * コンストラクタ
+   */
+  OutputConsoleVsSjis();
+
+  /**
+   * デストラクタ
+   */
+  virtual ~OutputConsoleVsSjis();
+
+  /**
+   * 文字列を出力する
+   */
+  void Print(std::string string);
+
+  /**
+   * 改行付き文字列を出力する
+   */
+  void PrintLine(std::string string);
+};
+
+}  // namespace console
+
+}  // namespace output
+
+}  // namespace mhl
+
+#endif  // MHL_OUTPUT_CONSOLE_OUTPUTCONSOLE_VS_SJIS_HPP_
