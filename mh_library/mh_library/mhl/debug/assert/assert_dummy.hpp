a72ffbd7aad937a5984f925fc1dfed34c7f7fb14
gdgdmh <gdgdkuro@gmail.com>
2021-06-05 22:50:45 +0900

    ダミー用Assertクラスとテストを追加

---
+++ mh_library/mh_library/mhl/debug/assert/assert_dummy.hpp
@@ -0,0 +1,55 @@
+﻿#ifndef MHL_DEBUG_ASSERT_MHL_ASSERT_DUMMY_HPP_
+#define MHL_DEBUG_ASSERT_MHL_ASSERT_DUMMY_HPP_
+
+#include <cassert>
+
+#include "iassert_processor.hpp"
+
+namespace mhl {
+
+namespace debug {
+
+namespace assert {
+
+/**
+ * @brief アサーションダミークラス
+ * 確認用に呼ばれたどうかの確認に使用する
+ *
+ */
+class AssertDummy : public IAssertProcessor {
+ public:
+  AssertDummy() : is_called_(false) {}
+
+  /**
+   * @brief デストラクタ
+   *
+   */
+  virtual ~AssertDummy() {}
+
+  /**
+   * @brief アサーション処理
+   *
+   */
+  virtual void Assert() override { is_called_ = true; }
+
+  /**
+   * @brief アサーションが呼ばれたかチェック
+   *
+   * @return true アサーションが呼ばれた
+   * @return false アサーションが呼ばれてない
+   */
+  virtual bool IsCalled() const { return is_called_; }
+
+  virtual void Reset() { is_called_ = false; }
+
+ private:
+  bool is_called_;  // アサーションが呼ばれたか判定するフラグ
+};
+
+}  // namespace assert
+
+}  // namespace debug
+
+}  // namespace mhl
+
+#endif  // MHL_DEBUG_ASSERT_MHL_ASSERT_DUMMY_HPP_
