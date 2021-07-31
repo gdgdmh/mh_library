8cea527a367626e2528f7adfb658d60a51834bee
gdgdmh <gdgdkuro@gmail.com>
2021-07-31 22:58:56 +0900

    DebugLogクラスを追加

---
+++ mh_library/mh_library/mhl/debug/service_locator/log/debug_log.hpp
@@ -0,0 +1,40 @@
+﻿#ifndef MHL_DEBUG_SERVICE_LOCATOR_LOG_DEBUG_LOG_HPP_
+#define MHL_DEBUG_SERVICE_LOCATOR_LOG_DEBUG_LOG_HPP_
+
+namespace mhl {
+namespace debug {
+namespace service_locator {
+namespace log {
+
+/**
+ * @brief ログ機能
+ *
+ */
+class DebugLog {
+ public:
+  /**
+   * @brief コンストラクタ
+   *
+   */
+  DebugLog();
+
+  /**
+   * @brief デストラクタ
+   *
+   */
+  virtual ~DebugLog();
+
+  /**
+   * @brief ログ出力
+   *
+   * @param format フォーマット文字列
+   * @param ... 可変引数パラメータ
+   */
+  void Log(const char* format, ...);
+};
+}  // namespace log
+}  // namespace service_locator
+}  // namespace debug
+}  // namespace mhl
+
+#endif  // MHL_DEBUG_SERVICE_LOCATOR_LOG_DEBUG_LOG_HPP_
