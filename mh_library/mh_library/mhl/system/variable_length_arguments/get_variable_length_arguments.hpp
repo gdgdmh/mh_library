1fe294e3a5237bbb0bee0a03ab25459e2b9a52b6
gdgdmh <gdgdkuro@gmail.com>
2021-08-09 12:47:57 +0900

    不要コードを削除

--- mh_library/mh_library/mhl/system/variable_length_arguments/get_variable_length_arguments.hpp
+++ mh_library/mh_library/mhl/system/variable_length_arguments/get_variable_length_arguments.hpp
@@ -41,65 +41,6 @@ class GetVariableLengthArguments {
   }
 };
 
-#if 0
-/**
- * @brief メルセンヌ・ツイスタ乱数
- *
- */
-class MtRand : public mhl::IRandGetable {
- public:
-  /**
-   * @brief コンストラクタ
-   *
-   */
-  MtRand();
-
-  /**
-   * @brief デストラクタ
-   *
-   */
-  virtual ~MtRand();
-
-  /**
-   * @brief 乱数の種を設定する
-   *
-   * @param seed 設定する乱数
-   */
-  void setSeed(uint64_t seed) override;
-
-  /**
-   * @brief 乱数の種を設定する
-   *
-   * @param seed 設定する乱数
-   */
-  void setSeed(uint32_t seed) override;
-
-  /**
-   * @brief 現在時刻から乱数を設定する
-   *
-   */
-  void setTimeSeed() override;
-
-  /**
-   * @brief 乱数を取得する
-   *
-   * @return int32_t 生成された乱数
-   */
-  int32_t getRandInt32(int32_t min, int32_t max) const override;
-
-  /**
-   * @brief 乱数を取得する
-   *
-   * @param min 最小値
-   * @param max 最大値
-   * @return uint32_t 生成された乱数
-   */
-  uint32_t getRandUint32(uint32_t min, uint32_t max) const override;
-
- private:
-  std::shared_ptr<std::mt19937_64> mt_;
-};
-#endif
 }  // namespace variable_length_arguments
 }  // namespace system
 }  // namespace mhl
