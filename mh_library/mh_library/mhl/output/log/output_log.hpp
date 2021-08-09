eedfbc044196004e87e5f51cfb37e3396ad9f48c
gdgdmh <gdgdkuro@gmail.com>
2021-08-09 12:48:47 +0900

    ログ出力インターフェースを追加、ログ出力コードを追加

---
+++ mh_library/mh_library/mhl/output/log/output_log.hpp
@@ -0,0 +1,54 @@
+﻿#ifndef MHL_OUTPUT_LOG_OUTPUT_LOG_HPP_
+#define MHL_OUTPUT_LOG_OUTPUT_LOG_HPP_
+
+#include <memory>
+#include <string>
+#include <ostream>
+
+#include "ilog_outputable.hpp"
+
+namespace mhl {
+
+namespace output {
+
+namespace log {
+
+// コンソール出力クラス
+class OutputLog : public mhl::output::log::ILogOutputable {
+ public:
+  /**
+   * @brief コンストラクタ
+   *
+   * @param file_name ファイル名
+   */
+  OutputLog(const std::string file_name);
+
+  /**
+   * デストラクタ
+   */
+  virtual ~OutputLog();
+
+  /**
+   * 文字列を出力する
+   */
+  void Print(std::string string);
+
+  /**
+   * 改行付き文字列を出力する
+   */
+  void PrintLine(std::string string);
+
+ public:
+  std::string file_path_;
+  std::shared_ptr<std::ofstream> out_stream_;
+  bool is_open_;
+  bool open_error_;
+};
+
+}  // namespace log
+
+}  // namespace output
+
+}  // namespace mhl
+
+#endif  // MHL_OUTPUT_LOG_OUTPUT_LOG_HPP_
