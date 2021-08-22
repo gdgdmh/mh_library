48271f104057e3a2ac6a94ea8ac083c87be03fbc
gdgdmh <gdgdkuro@gmail.com>
2021-08-22 19:32:19 +0900

    ファイル削除とファイル存在チェックを作成

---
+++ mh_library/mh_library/mhl/system/file/file_exist_checker_win.cpp
@@ -0,0 +1,34 @@
+﻿#include "file_exist_checker_win.hpp"
+
+#include <Shlwapi.h>
+
+#pragma comment(lib, "Shlwapi.lib")
+
+/**
+ * @brief コンストラクタ
+ *
+ */
+mhl::system::file::FileExistCheckerWin::FileExistCheckerWin() {}
+
+/**
+ * @brief デストラクタ
+ *
+ */
+mhl::system::file::FileExistCheckerWin::~FileExistCheckerWin() {}
+
+/**
+ * @brief ファイルが存在するかチェック
+ *
+ * @param str ファイルパス
+ * @return true ファイルが存在する
+ * @return false ファイルが存在しない
+ */
+bool mhl::system::file::FileExistCheckerWin::IsExists(const std::string& str) {
+  if (str.empty()) {
+    return false;
+  }
+  if (PathFileExistsA(str.c_str())) {
+    return true;
+  }
+  return false;
+}
---
+++ mh_library/mh_library/mhl/system/file/file_exist_checker_win.hpp
@@ -0,0 +1,42 @@
+﻿#ifndef MHL_SYSTEM_FILE_FILE_EXIST_CHECKER_HPP_
+#define MHL_SYSTEM_FILE_FILE_EXIST_CHECKER_HPP_
+
+#include "ifile_exist_checker.hpp"
+
+namespace mhl {
+namespace system {
+namespace file {
+
+/**
+ * @brief ファイル存在チェッククラス
+ * 存在チェックはいろいろな方法があるがWindows専用のチェック方法をとる
+ *
+ */
+class FileExistCheckerWin : public IFileExistChecker {
+ public:
+  /**
+   * @brief コンストラクタ
+   *
+   */
+  FileExistCheckerWin();
+
+  /**
+   * @brief デストラクタ
+   *
+   */
+  virtual ~FileExistCheckerWin() override;
+
+  /**
+   * @brief ファイルが存在するかチェック
+   *
+   * @param str ファイルパス
+   * @return true ファイルが存在する
+   * @return false ファイルが存在しない
+   */
+  bool IsExists(const std::string& str) override;
+};
+}  // namespace file
+}  // namespace system
+}  // namespace mhl
+
+#endif  // MHL_SYSTEM_FILE_FILE_EXIST_CHECKER_HPP_
