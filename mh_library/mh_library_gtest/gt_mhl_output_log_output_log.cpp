0d32b255d1a12e1bbdc005bceb2b705469615fe1
gdgdmh <gdgdkuro@gmail.com>
2021-10-02 23:35:39 +0900

    テスト内容を仮定義

---
+++ mh_library/mh_library_gtest/gt_mhl_output_log_output_log.cpp
@@ -0,0 +1,37 @@
+﻿
+#include "a_pch.h"
+#include <mhl/output/log/output_log.hpp>
+#include <mhl/system/file/file_deleter.hpp>
+
+namespace mhl {
+namespace output {
+namespace log {
+
+TEST(OutputLog, Constructor) {}
+
+/**
+ * @brief test
+ *
+ */
+TEST(OutputLog, Initialize) {
+  //const std::string file_name = "gtest_output_log_initialize.txt";
+  // Arrange
+  //mhl::system::file::FileDeleter fd;
+  //fd.Delete(file_name);
+
+  // Act
+  // Assert
+  // EXPECT_EQ(fd.Delete(path), true);
+}
+
+TEST(OutputLog, Finalize) {}
+
+TEST(OutputLog, Print) {}
+
+TEST(OutputLog, PrintLine) {}
+
+TEST(OutputLog, IsInitialize) {}
+
+}  // namespace log
+}  // namespace output
+}  // namespace mhl
---
+++ mh_library/mh_library_gtest/gt_mhl_output_log_output_logs.cpp
@@ -0,0 +1,24 @@
+﻿
+#include "a_pch.h"
+#include "mhl/output/log/output_logs.hpp"
+
+namespace mhl {
+namespace output {
+namespace log {
+
+/**
+ * @brief test
+ *
+ */
+TEST(OutputLogs, Initialize) {
+  // Arrange
+  // mhl::system::singleton::SingletonCleanup::Finalize();
+  // Act
+  // bool result = mhl::system::singleton::SingletonCleanup::Initialize();
+  // Assert
+  // EXPECT_EQ(fd.Delete(path), true);
+}
+
+}  // namespace log
+}  // namespace output
+}  // namespace mhl
