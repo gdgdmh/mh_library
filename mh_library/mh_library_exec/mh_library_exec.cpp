21cae169166d86ee30322761a99385a7f19cf4e4
gdgdmh <gdgdkuro@gmail.com>
2021-12-19 10:12:04 +0900

    相対パスに変更

--- mh_library/mh_library_exec/mh_library_exec.cpp
+++ mh_library/mh_library_exec/mh_library_exec.cpp
@@ -1,14 +1,12 @@
 ﻿#include <memory>
-
-#include "mhl/output/console/iconsole_outputables.hpp"
-#include "mhl/output/console/output_consoles.hpp"
-#include "mhl/output/console/output_console.hpp"
-#include "mhl/output/console/output_console_vs_sjis.hpp"
-#include "mhl/test_code/test_mhl.hpp"
-#include "mhl/test_program/test_mhl_test_program.hpp"
+#include <mhl/output/console/iconsole_outputables.hpp>
+#include <mhl/output/console/output_console.hpp>
+#include <mhl/output/console/output_console_vs_sjis.hpp>
+#include <mhl/output/console/output_consoles.hpp>
+#include <mhl/test_code/test_mhl.hpp>
+#include <mhl/test_program/test_mhl_test_program.hpp>
 
 int main() {
-  
   std::shared_ptr<mhl::output::console::IConsoleOutputables> output(
       new mhl::output::console::OutputConsoles());
   std::shared_ptr<mhl::output::console::IConsoleOutputable> output_console(
@@ -29,9 +27,9 @@ int main() {
   return 0;
 }
 
-// mh_library_exec.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+// mh_library_exec.cpp : このファイルには 'main'
+// 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
 //
-
 
 /*
 #include <iostream>
@@ -45,10 +43,14 @@ int main()
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
 // プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
 
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+// 作業を開始するためのヒント:
+//    1. ソリューション エクスプローラー
+//    ウィンドウを使用してファイルを追加/管理します
 //   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
 //   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
 //   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード
+//   ファイルを作成するか、[プロジェクト] > [既存の項目の追加]
+//   と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] >
+//   [プロジェクト] と移動して .sln ファイルを選択します
