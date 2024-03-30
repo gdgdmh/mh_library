e6b4c1ce7474ab4dfab7f5c30db5d12877041fa3
gdgdmh <gdgdkuro@gmail.com>
2024-03-30 22:50:42 +0900

    stateの追加

---
+++ mh_library/mh_library_gtest/gt_mhl_state_state.cpp
@@ -0,0 +1,95 @@
+﻿
+#include "a_pch.h"
+#include "mhl/state/state.hpp"
+
+namespace mhl {
+namespace state {
+
+enum class TestState : int32_t {
+  None,
+  First,
+  Second,
+  Third,
+  Max
+};
+
+/**
+ * @brief コンストラクタステータスデフォルト値チェック
+ *
+ */
+TEST(State, ConstructorDefaultState) {
+  mhl::State<TestState> s;
+  EXPECT_EQ(s.GetState(), TestState::None);
+}
+
+/**
+ * @brief コンストラクタリクエストステータスデフォルト値チェック
+ *
+ */
+TEST(State, ConstructorDefaultRequestState) {
+  mhl::State<TestState> s;
+  EXPECT_EQ(s.GetRequestState(), TestState::None);
+}
+
+/**
+ * @brief ステータス変更リクエスト None設定
+ * 
+ */
+TEST(State, RequestChangeSetNone) {
+  mhl::State<TestState> s;
+  s.RequestChange(TestState::None);
+  EXPECT_EQ(s.GetState(), TestState::None);
+  EXPECT_EQ(s.GetRequestState(), TestState::None);
+}
+
+/**
+ * @brief ステータス変更リクエスト First設定
+ * 
+ */
+TEST(State, RequestChangeSetFirst) {
+  mhl::State<TestState> s;
+  s.RequestChange(TestState::First);
+  EXPECT_EQ(s.GetState(), TestState::None);
+  EXPECT_EQ(s.GetRequestState(), TestState::First);
+}
+
+/**
+ * @brief 即時ステータス変更リクエスト First設定
+ * 
+ */
+TEST(State, DoImmidateRequestChangeSetFirst) {
+  mhl::State<TestState> s;
+  s.DoImmediateChangeRequest(TestState::First);
+  EXPECT_EQ(s.GetState(), TestState::First);
+  EXPECT_EQ(s.GetRequestState(), TestState::First);
+}
+
+/**
+ * @brief 変更リクエスト適用 First設定
+ * 
+ */
+TEST(State, ApplyChangeFirst) {
+  mhl::State<TestState> s;
+  s.RequestChange(TestState::First);
+
+  bool result = s.ApplyChangeRequest();
+  EXPECT_EQ(s.GetState(), TestState::First);
+  EXPECT_EQ(s.GetRequestState(), TestState::First);
+  EXPECT_EQ(result, true);
+}
+
+/**
+ * @brief 変更リクエスト適用 変更なし
+ * 
+ */
+TEST(State, ApplyChangeNoChange) {
+  mhl::State<TestState> s;
+  s.DoImmediateChangeRequest(TestState::First);
+  bool result = s.ApplyChangeRequest();
+  EXPECT_EQ(s.GetState(), TestState::First);
+  EXPECT_EQ(s.GetRequestState(), TestState::First);
+  EXPECT_EQ(result, false);
+}
+
+}  // namespace bit
+}  // namespace mhl
