e2781a4eb7042450335fe95e00e9debb0ff9fc27
gdgdmh <gdgdkuro@gmail.com>
2024-05-05 08:38:49 +0900

    コードを追加
    basis_point

---
+++ mh_library/mh_library/mhl/system/math/probability/basis_point.cpp
@@ -0,0 +1,45 @@
+﻿#include "basis_point.hpp"
+
+#include "../../rand/irand_getable.hpp"
+#include "../../../exception/argument_exception.hpp"
+
+/**
+ * @brief コンストラクタ
+ *
+ */
+mhl::system::math::probability::BasisPoint::BasisPoint() {}
+
+/**
+ * @brief デストラクタ
+ *
+ */
+mhl::system::math::probability::BasisPoint::~BasisPoint() {}
+
+/**
+ * @brief 乱数クラスを設定する
+ *
+ * @param rand 乱数クラス
+ */
+void mhl::system::math::probability::BasisPoint::SetRand(
+    std::unique_ptr<mhl::IRandGetable>&& rand) {
+      rand_ = std::move(rand);
+}
+
+/**
+ * @brief 配列の中から1つの要素を選ぶ
+ *
+ * @param target 対象の配列
+ * @return int32_t
+ */
+int32_t mhl::system::math::probability::BasisPoint::Choose(
+    const std::vector<int32_t>& target) {
+      if (!rand_.get()) {
+        throw mhl::exception::ArgumentException("Rand Not Set", 1);
+      }
+
+      if (target.size() != 10000) {
+        throw mhl::exception::ArgumentException("target Size Not 10000", 2);        
+      }
+
+      return 0;
+    }
